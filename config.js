3a444156080ddb3068445f6f2f5e4216cc973dbf
saedmp <68201419+saedmp@users.noreply.github.com>
2020-10-28 05:54:54 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "Njk3NDI5MTAxNzczODQ4NTk4.Xo3Jdw.KG9Ufs2onYN5bMCJn3xrXLlFRQY", //تعديل مهم توكن بوتك
+  TOKEN: "Njk3NDI5MTAxNzczODQ4NTk4.Xo3Ogg.WBFPeup1TPLnhAP8HAgTQ6ClmoU", //تعديل مهم توكن بوتك
   YT_API_KEY: "api ",
   prefix: "=",
   devs: ["470003692322553857"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
